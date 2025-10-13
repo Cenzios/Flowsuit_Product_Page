ccb1f6da35e27a00fa3afe5a1e985ee425552b4d
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-13 10:38:30 +0530

    Update background color and icon color in CutterSection, PressingSection, and PrintingSection for improved visibility

--- src/components/CutterSection.tsx
+++ src/components/CutterSection.tsx
@@ -112,11 +112,8 @@ const CutterSectionManagement = ({}: CutterSectionManagementProps) => {
                     whileHover={{ scale: 1.15, rotate: 5 }}
                     transition={{ duration: 0.3 }}
                   >
-                    <div className="w-5 h-5 rounded-full bg-green-500/10 flex items-center justify-center">
-                      <Check
-                        className="w-3.5 h-3.5 text-green-500 stroke-[5]
-"
-                      />
+                    <div className="w-5 h-5 rounded-full bg-[#46A834] flex items-center justify-center">
+                      <Check className="w-3.5 h-3.5 text-white stroke-[5]" />
                     </div>
                   </motion.div>
                   <p
