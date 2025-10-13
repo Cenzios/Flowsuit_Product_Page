c6b43e10bcdaba9d012593aceb553f55855216b8
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-13 10:39:00 +0530

    Update check icon colors in CustomerDashboardSection for improved visibility

--- src/components/StepsForOrders.tsx
+++ src/components/StepsForOrders.tsx
@@ -173,11 +173,8 @@ const CustomerDashboardSection = ({}: CustomerDashboardSectionProps) => {
                       whileHover={{ scale: 1.15, rotate: 5 }}
                       transition={{ duration: 0.3 }}
                     >
-                      <div className="w-5 h-5 rounded-full bg-green-500/10 flex items-center justify-center">
-                        <Check
-                          className="w-3.5 h-3.5 text-green-500 stroke-[5]
-"
-                        />
+                      <div className="w-5 h-5 rounded-full bg-[#46A834] flex items-center justify-center">
+                        <Check className="w-3.5 h-3.5 text-white stroke-[5]" />
                       </div>
                     </motion.div>
                     <p
@@ -250,11 +247,8 @@ const CustomerDashboardSection = ({}: CustomerDashboardSectionProps) => {
                       whileHover={{ scale: 1.15, rotate: 5 }}
                       transition={{ duration: 0.3 }}
                     >
-                      <div className="w-5 h-5 rounded-full bg-green-500/10 flex items-center justify-center">
-                        <Check
-                          className="w-3.5 h-3.5 text-green-500 stroke-[5]
-"
-                        />
+                      <div className="w-5 h-5 rounded-full bg-[#46A834] flex items-center justify-center">
+                        <Check className="w-3.5 h-3.5 text-white stroke-[5]" />
                       </div>
                     </motion.div>
                     <p
